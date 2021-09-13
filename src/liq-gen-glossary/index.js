87f80c50373c872c83808e21ee931b9da1b84f02
Zane Rockenbaugh <zane@liquid-labs.com>
2021-09-13 14:14:08 -0500

    now collects and lists terms by domain

--- src/liq-gen-glossary/index.js
+++ src/liq-gen-glossary/index.js
@@ -1,39 +1,5 @@
-import { generateGlossary } from './generate-glossary'
+import { printGlossary } from './generate-glossary'
 
 // TODO: the use of an 'index' file is a little misleading in this case. We want to build a tool, not a 'bundle'. We would like to update the catalyst-scripts (which need to be reworked) to be a bit more flexible.
 
-const dashCase = (term) => {
-  return term.toLowerCase().replace(/[^a-z0-9]/ig, '-')
-}
-
-const definitionFiles = [...process.argv.slice(2)]
-
-const allTerms = generateGlossary({ definitionFiles })
-
-const caseInsensitiveSort = function(a, b) {
-  a = a.toLowerCase()
-  b = b.toLowerCase()
-  if (a < b) {
-    return -1
-  }
-  else if (a > b) {
-    return 1
-  }
-  else { // names must be equal
-    return 0
-  }
-}
-
-// Note: 'console.log(...)' adds it's own newline, so the '\n' in the following creates a blank line.
-console.log('# Glossary\n')
-console.log('<dl>\n')
-
-const keys = Object.keys(allTerms) || []
-keys.sort(caseInsensitiveSort)
-
-for (const term of keys) {
-  console.log(`<dt id="${dashCase(term)}">${term}</dt>`)
-  console.log(`<dd>\n\n${allTerms[term]}\n\n</dd>\n\n`) // the blank line tells Markdown to process the contents.
-}
-
-console.log('</dl>')
+printGlossary()
