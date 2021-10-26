8ab52682cbaf7ac14495601a008b9fc2d5e318c5
Zane Rockenbaugh <zane@liquid-labs.com>
2021-10-26 18:52:38 -0500

    re-org tests to group qualifier tests

--- js/roles/test/AttachedRole.test.js
+++ js/roles/test/AttachedRole.test.js
@@ -7,11 +7,6 @@ describe('AttachedRole', () => {
     org = new Organization('./js/test-data', './js/staff/test/staff.json')
   })
 
-  test('detects invalid qualifications', () => {
-    expect(() => new Organization('./js/test-data', './js/staff/test/invalid_qualifier_staff.json'))
-      .toThrow(/non-qualifiable role.*CTO.*ceo@foo\.com/)
-  })
-
   test.each`
   email | roleName
   ${'dev@foo.com'} | ${'Developer'}
@@ -20,20 +15,27 @@ describe('AttachedRole', () => {
     expect(org.getStaff().get(email).getAttachedRole(roleName).getName()).toBe(roleName)
   })
 
-  test.each`
-  email | roleName | isQualifiable
-  ${'dev@foo.com'} | ${'Developer'} | ${true}
-  ${'ceo@foo.com'}| ${'CEO'} | ${false}
-  `('\'$email\' role \'$roleName\' is qualifiable: $isQualifiable', ({ email, roleName, isQualifiable }) => {
-    expect(org.getStaff().get(email).getAttachedRole(roleName).isQualifiable()).toBe(isQualifiable)
-  })
+  describe('qualifiers', () => {
+    test('are detected when invalid', () => {
+      expect(() => new Organization('./js/test-data', './js/staff/test/invalid_qualifier_staff.json'))
+        .toThrow(/non-qualifiable role.*CTO.*ceo@foo\.com/)
+    })
 
-  test.each`
-  email | roleName | qualifier
-  ${'uidev@foo.com'} | ${'Developer'} | ${'UI'}
-  ${'dev@foo.com'} | ${'Developer'} | ${null}
-  ${'ceo@foo.com'} | ${'CEO'} | ${null}
-  `('\'$email\' role \'$roleName\' has \'$qualifier\' qualifier.', ({ email, roleName, qualifier }) => {
-    expect(org.getStaff().get(email).getAttachedRole(roleName).getQualifier()).toBe(qualifier)
+    test.each`
+    email | roleName | isQualifiable
+    ${'dev@foo.com'} | ${'Developer'} | ${true}
+    ${'ceo@foo.com'}| ${'CEO'} | ${false}
+    `('\'$email\' role \'$roleName\' is qualifiable: $isQualifiable', ({ email, roleName, isQualifiable }) => {
+      expect(org.getStaff().get(email).getAttachedRole(roleName).isQualifiable()).toBe(isQualifiable)
+    })
+
+    test.each`
+    email | roleName | qualifier
+    ${'uidev@foo.com'} | ${'Developer'} | ${'UI'}
+    ${'dev@foo.com'} | ${'Developer'} | ${null}
+    ${'ceo@foo.com'} | ${'CEO'} | ${null}
+    `('\'$email\' role \'$roleName\' has \'$qualifier\' qualifier.', ({ email, roleName, qualifier }) => {
+      expect(org.getStaff().get(email).getAttachedRole(roleName).getQualifier()).toBe(qualifier)
+    })
   })
 })
