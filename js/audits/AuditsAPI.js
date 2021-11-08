8f8287ee9811c1fff16f411678b1058801e80dbd
Zane Rockenbaugh <zane@liquid-labs.com>
2021-11-08 15:50:33 -0600

    regularized idiomatic Audits classes

--- js/audits/AuditsAPI.js
+++ js/audits/AuditsAPI.js
@@ -1,20 +1,10 @@
-const AuditsAPI = class {
+import { Audits } from './Audits'
+
+const AuditsAPI = class extends Audits {
   constructor(org) {
+    super(org.innerState.audits)
+    
     this.org = org
-    
-    this.items = org.innerState.audits
-    this.index = this.items.reduce((index, entry) => { index[entry.name] = entry; return index }, {})
-  }
-  
-  list() { return this.items.slice() }
-  
-  get(name, { required = false } = {}) {
-    const result = this.index[name]
-    if (required === true && result === undefined) {
-      throw new Error(`Did not find required audit '${name}'.`)
-    }
-    
-    return result
   }
 }
 
