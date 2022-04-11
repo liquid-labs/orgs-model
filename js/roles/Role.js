b0e7e2811c9a3db1dab0506b0449809356e39b88
Zane Rockenbaugh <zane@liquid-labs.com>
2022-04-11 18:13:55 -0500

    lint fixes

--- js/roles/Role.js
+++ js/roles/Role.js
@@ -16,14 +16,10 @@ bindCreationConfig({
   dataCleaner   : (data) => { delete data.id; return data },
   dataFlattener : (data) => {
     const { implies, aliases, duties, matcher } = data
-    if (data.implies)
-      data.implies = `${implies.name};${implies.mngrProtocol}`
-    if (data.aliases)
-      data.aliases = aliases.join('; ')
-    if (data.description)
-      data.description = data.description.link
-    if (data.duties)
-      data.duties = duties.map((d) => d.description).join('; ')
+    if (data.implies) { data.implies = `${implies.name};${implies.mngrProtocol}` }
+    if (data.aliases) { data.aliases = aliases.join('; ') }
+    if (data.description) { data.description = data.description.link }
+    if (data.duties) { data.duties = duties.map((d) => d.description).join('; ') }
     if (data.matcher) {
       const { pattern, antiPattern, qualifierGroup } = matcher
       data.matcher = `/${pattern}/`
