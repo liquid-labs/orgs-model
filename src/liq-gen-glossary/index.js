48b6aea5ad8cdfcd3fe6404122ce459cf2813d39
Zane Rockenbaugh <zane@liquid-labs.com>
2022-04-24 17:36:59 -0500

    migrated glossary generation tool to liq-policy

--- src/liq-gen-glossary/index.js
+++
@@ -1,5 +0,0 @@
-import { printGlossary } from './generate-glossary'
-
-// TODO: the use of an 'index' file is a little misleading in this case. We want to build a tool, not a 'bundle'. We would like to update the catalyst-scripts (which need to be reworked) to be a bit more flexible.
-
-printGlossary()
