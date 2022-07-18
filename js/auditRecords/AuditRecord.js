b496b181a507a5c1d6593e56a5a4f883a50ee869
Zane Rockenbaugh <zane@liquid-labs.com>
2022-07-17 22:48:39 -0500

    noted future todo

--- js/auditRecords/AuditRecord.js
+++ js/auditRecords/AuditRecord.js
@@ -2,6 +2,14 @@ import { Item, bindCreationConfig } from '../lib/Item'
 
 const AuditRecord = class extends Item { }
 
+/* TODO: move 'stauts', 'info', 'todo', and 'asOf' in 'auditRecords' to 'records', an array sorted by timestamp.; eg:
+* {
+*   id,
+*   ...,
+*   records: [ { status, info, todo, asOf }, ... ]
+}
+*/
+
 bindCreationConfig({
   itemClass    : AuditRecord,
   itemName     : 'audit record',
